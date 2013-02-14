0347314736d77ebad3281a045fc82e7c1a908d47
Bogdan Matican <bogdanmatican@gmail.com>
2013-02-14 15:44:56 +0000

    Added proper nonce mechanics. TODO: test them

--- src/common/nonce.ml
+++ src/common/nonce.ml
@@ -1,22 +1,51 @@
-type time = int
-type random = int
-type t = time * random
+type t = {
+  since_epoch : int;
+  random : int;
+}
+exception InvalidLengthException
+
+let offset_past = 60 * 60 * 24 * 2
+let offset_future = 60 * 60
 
 let random_size = 4 (* INT SIZE *)
+let int_size = 4
+
 let random_int () = 
   let rand = Primitives.Random.generate random_size in
   (Utils.four_octet_decode rand)
 
-let create_exact time rand = (time, rand)
+let now () = 
+  (int_of_float (Unix.gettimeofday ())) / 1000
 
-let create_timed time = 
-  let rand = random_int () in
-  create_exact time rand
+let create_exact since_epoch random = 
+  {
+    since_epoch = since_epoch;
+    random = random;
+  }
+
+let create_timed since_epoch = 
+  let random = random_int () in
+  create_exact since_epoch random
 
 let create_random () = 
-  let rand = random_int () in
-  let time = int_of_float (Unix.gettimeofday ()) in
-  create_exact time rand
+  let random = random_int () in
+  let since_epoch = now () in
+  create_exact since_epoch random
 
-(*TODO*)
-let is_recent nonce = false
+let from_string str =
+  if String.length str != random_size + int_size
+  then raise InvalidLengthException
+  else
+    let random = int_of_string (String.sub str 0 random_size) in
+    let since_epoch = int_of_string (String.sub str random_size int_size) in
+    create_exact since_epoch random
+
+let to_string nonce =
+  let random = nonce.random in
+  let since_epoch = nonce.since_epoch in
+  (Utils.four_octet_encode random) ^ (Utils.four_octet_encode since_epoch)
+
+let is_recent nonce =
+  let current = now () in
+  let diff = current - nonce.since_epoch in
+  (diff < offset_past) && (diff > -offset_future)
--- src/common/nonce.mli
+++ src/common/nonce.mli
@@ -1,9 +1,11 @@
 type t
-type time = int
-type random = int
+exception InvalidLengthException
 
-val create_exact : time -> random -> t
-val create_timed : time -> t
+val create_exact : int -> int -> t
+val create_timed : int -> t
 val create_random : unit -> t
+
+val from_string : string -> t
+val to_string : t -> string
 
 val is_recent : t -> bool
